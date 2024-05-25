d0a5a44a3dbe8841ae082d9b15dbc421bdadd5f1
Louis Aussedat <aussedat.louis@gmail.com>
2024-05-26 00:06:14 +0200

    🎨 (lint): format config file

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,37 +1,37 @@
-const { group } = require("console");
+const { group } = require('console');
 
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react-hooks/recommended",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
   ],
-  ignorePatterns: ["dist", ".eslintrc.cjs"],
-  parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh", "simple-import-sort"],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh', 'simple-import-sort'],
   rules: {
-    "react-refresh/only-export-components": [
-      "warn",
+    'react-refresh/only-export-components': [
+      'warn',
       { allowConstantExport: true },
     ],
-    "simple-import-sort/imports": [
-      "error",
+    'simple-import-sort/imports': [
+      'error',
       {
         groups: [
           // Packages. `react` related packages come first.
-          ["^react", "^@?\\w"],
+          ['^react', '^@?\\w'],
           // Internal packages.
-          ["^(@|@data|@domain|@types|@view)(/.*|$)"],
+          ['^(@|@data|@domain|@types|@view)(/.*|$)'],
           // Other relative imports. Put same-folder imports and `.` last.
-          ["^\\./(?=.*/)(?!/?$)", "^\\.(?!/?$)", "^\\./?$"],
+          ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
           // Style imports.
-          ["^.+\\.s?css$"],
+          ['^.+\\.s?css$'],
         ],
       },
     ],
-    "simple-import-sort/exports": "error",
-    "no-restricted-imports": ["error", { patterns: ["../*"] }],
+    'simple-import-sort/exports': 'error',
+    'no-restricted-imports': ['error', { patterns: ['../*'] }],
   },
 };
