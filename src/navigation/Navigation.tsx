aa0646b6dec950569007f11cf3ed39192b537181
Louis Aussedat <aussedat.louis@gmail.com>
2024-05-26 11:16:26 +0200

    💄: add antd dependency

--- src/navigation/Navigation.tsx
+++ src/navigation/Navigation.tsx
@@ -1,10 +1,31 @@
 import React from 'react';
+import { Tabs, TabsProps } from 'antd';
 
 import { Dashboard } from '@/view/dashboard/Dashboard';
+
+const items: TabsProps['items'] = [
+  {
+    key: 'dashboard',
+    label: 'Dashboard',
+  },
+  {
+    key: 'jails',
+    label: 'Jails',
+  },
+  {
+    key: 'bans',
+    label: 'Bans',
+  },
+  {
+    key: 'history',
+    label: 'History',
+  },
+];
 
 export const Navigation: React.FC = () => {
   return (
     <>
+      <Tabs defaultActiveKey="1" items={items} onChange={() => {}} centered />
       <Dashboard />
     </>
   );
